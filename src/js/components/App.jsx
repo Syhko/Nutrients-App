2aa4b74bde505e13d4d7157218b4ce37c4544283
Alexandre Grauby <alexandregrauby@gmail.com>
2019-02-12 14:17:18 +0100

    Adding logos and links to GitHub and USDA API

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -4,6 +4,9 @@ import { connect } from "react-redux";
 import List from "./List";
 import Form from "./Form";
 import './App.css';
+import Syhko_logo from '../../logos/Syhko_logo.png';
+import USDA_logo from '../../logos/USDA_logo.png';
+import GitHub_logo from '../../logos/GitHub_logo.png';
 
 class App extends Component {
 
@@ -27,7 +30,29 @@ class App extends Component {
 
     return(
       <div className="app">
+        <div className="USDA-content">
+          <p className="text">Powered by</p>
+          <a href="https://ndb.nal.usda.gov/ndb/search/list?home=true" target="_blank" rel="noopener noreferrer">
+            <img
+              src={USDA_logo}
+              width="100"
+              alt="USDA_logo"
+            />
+          </a>
+        </div>
+        <a href="https://github.com/Syhko" target="_blank" rel="noopener noreferrer">
+          <img
+            className="github-logo"
+            src={GitHub_logo}
+            width="100"
+            alt="GitHub_logo"
+          />
+        </a>
         <div className="header">
+          <img
+            src={Syhko_logo}
+            alt="Syhko_logo"
+          />
           <h2>Syhko Food App</h2>
         </div>
         <div className="body">
